9f306a13a2e0af3e07959008a115b37f14026046
Joby Mathew <jobymathew.mec@gmail.com>
2020-04-18 13:52:27 +0530

    backend fixes

--- weather/urls.py
+++ weather/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='weather'),
+    path('delete/<str:pk>/',views.delete,name='delete'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 import requests
 from .models import City
 from .forms import CityForm
@@ -30,3 +30,9 @@ def index(request):
     context = {'weather_data': weather_data, 'form':form}
 
     return render(request,'weather/weather.html',context)
+
+def delete(request,pk):
+    city = get_object_or_404(City, pk=pk)
+    city.delete()
+    return redirect('/')
+        
